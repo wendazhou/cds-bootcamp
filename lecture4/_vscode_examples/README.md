c29789fb3ff69992a6ab6dd1abda316850df7547
Wenda Zhou <wz2247@nyu.edu>
2021-09-23 06:23:57 -0400

    add cpu test + doc for .vscode setup

---
+++ lecture4/_vscode_examples/README.md
@@ -0,0 +1,9 @@
+# Vscode example configuration files
+
+This folder contains example configuration files from vscode when setting up the test explorer.
+When setting up vscode interactively, these files would appear in the `.vscode/` folder.
+
+Note that to specify environment variables when debugging, we make use of the launch configuration
+functionality of vscode. Details on how to configure the vscode debugger for python tests
+may be found [here](https://code.visualstudio.com/docs/python/testing#_debug-tests).
+
--- lecture4/tests/test_indexing.py
+++ lecture4/tests/test_indexing.py
@@ -1,3 +1,12 @@
+"""Example failing tests and debugging CUDA
+
+These tests below fail because the label is out of bound for the current model.
+The model expects to classify CIFAR-10, which has 10 labels, which would be labelled
+from 0 to 9. However, I have created a fake label 10, and passed that in to the model.
+This will introduce an error when computing the loss.
+
+"""
+
 import torch
 
 from bootcamp import train_lr
@@ -13,6 +22,22 @@ def test_simple_convnet_integration():
     optim = torch.optim.SGD(model.parameters(), lr=1e-2)
 
     scaler = torch.cuda.amp.GradScaler()
+    dataloader = train_lr.InMemoryDataloader((images, labels), batch_size=2)
+
+    result = train_lr.train_epoch(model, optim, scaler, dataloader)
+    assert len(result) == 2
+
+
+def test_simple_convnet_integration_cpu():
+    device = 'cpu'
+
+    labels = torch.tensor([0, 10, 5, 2]).to(device)
+    images = torch.randn([4, 3, 32, 32]).to(device, memory_format=torch.channels_last)
+
+    model = train_lr.SimpleConvNet().to(device=device, memory_format=torch.channels_last)
+    optim = torch.optim.SGD(model.parameters(), lr=1e-2)
+
+    scaler = None # Scaler not enable on CPU
     dataloader = train_lr.InMemoryDataloader((images, labels), batch_size=2)
 
     result = train_lr.train_epoch(model, optim, scaler, dataloader)
